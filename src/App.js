ab60998a2262233366254fa2bd2f19106352ca3a
Bgstatic <bilgehangecici@gmail.com>
2020-09-08 19:04:02 +0300

    Firebbase integration.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import './App.css';
+import Todo from './components/Todo';
+import db from './firebase/firebase';
+import firebase from 'firebase';
 
 function App() {
+
+  const [todos, setTodos] = useState(['abc', 'def']);
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+     
+    setTodos(snapshot.docs.map(doc => doc.data().todo))
+      
+    });
+
+  }, [])
+
+  const addTodo = (event) => {
+
+    event.preventDefault();
+
+    db.collection('todos').add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+
+    })
+    setInput('');
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React ToDo App</h1>
+      <form>
+        <FormControl>
+          <InputLabel>Add Items</InputLabel>
+          <Input type="text" value={input} onChange={event => setInput(event.target.value)} placeholder="Add Item..." />
+        </FormControl>
+        <Button disabled={!input} type="submit" onClick={addTodo} color="primary" variant="contained">Add</Button>
+      </form>
+
+      <ul>
+        {todos.map(todo => (
+
+          <Todo todo={todo} />
+
+        ))}
+      </ul>
     </div>
   );
 }
